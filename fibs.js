23c642c5d11b4d8698c7718bf9e0240bc3f1d978
mkhavin-avenues <michael_khavin@avenues-ny.org>
2024-12-16 09:55:56 -0500

    Update fibs.js

--- fibs.js
+++ fibs.js
@@ -1,19 +1,32 @@
-const memo = [];
+let fibs = [0, 1, 1, 2];
+let n = 1;
 
-function fib(n) {
-    if (memo[n] !== undefined) {
-        return memo[n];
-    }
-
-    if (n <= 1) {
-        return n; 
-    }
-
-
-    const result = fib(n - 1) + fib(n - 2);
-
-    memo[n] = result;
-    return result;
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+  textAlign(CENTER, CENTER);
 }
 
-console.log(fib(10)); 
+function draw() {
+  background(220);
+  text(`${n}th Fibonacci number: ${fib(n)}`, width / 2, height / 2);
+}
+
+function keyPressed() {
+  if (key === " ") {
+    n += 1;
+    redraw();
+  }
+}
+
+function fib(n) {
+  if (n < 1) {
+    return 0;
+  }
+
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
